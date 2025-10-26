a8ef94d213986fd1b17908b49f821e9ffc877cfc
Manjunath hadpad <hadpadm@gmail.com>
2025-10-26 12:28:25 +0530

    Initial commit - fresh setup

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,7 @@
-
-
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/ponda-kannada-sangh/',  // <- use your repo name here
+  base: '/ponda-kannada-sangh/', // must match your repo name with trailing slash
   plugins: [react()],
 });
-
